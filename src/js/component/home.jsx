32a6d33e6546f48ce5933db2cd595b49c0438660
josemoracard <jose.cardinales@gmail.com>
2023-01-13 15:51:16 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,53 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import "/workspace/react-hello/src/styles/index.css";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+const Home = (props) => {
+	const [seconds, setSeconds] = useState(props.seconds);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+		  setSeconds(seconds => seconds + 1);
+		}, 1000);
+		return () => clearInterval(interval);
+	  }, []);
+	
+	  const hours = Math.floor(seconds / 3600);
+	  const minutes = Math.floor((seconds % 3600) / 60);
+	  const remainingSeconds = seconds % 60;
+	
+	  return (
+		<div className="d-flex justify-content-center mt-3" style={{height: "10rem"}}>
+			<div className="card me-3">
+				<div className="card-body d-flex align-items-center justify-content-center bg-dark" style={{width: "7rem", color: "white"}}><i className="fa-solid fa-stopwatch fa-5x"></i></div>
+			</div>
+
+			<div className="card me-3">
+		  		<div className="card-body d-flex align-items-center justify-content-center p-0 bg-dark" style={{width: "7rem", fontSize:"5rem", color: "white"}}>{Math.floor(hours / 10)}</div>
+			</div>
+
+			<div className="card me-5">
+		  		<div className="card-body d-flex align-items-center justify-content-center p-0 bg-dark" style={{width: "7rem", fontSize:"5rem", color: "white"}}>{hours % 10}</div>
+			</div>
+
+			<div className="card me-3">
+		  		<div className="card-body d-flex align-items-center justify-content-center p-0 bg-dark" style={{width: "7rem", fontSize:"5rem", color: "white"}}>{Math.floor(minutes / 10)}</div>
+			</div>
+
+			<div className="card me-5">
+		  		<div className="card-body d-flex align-items-center justify-content-center p-0 bg-dark" style={{width: "7rem", fontSize:"5rem", color: "white"}}>{minutes % 10}</div>
+			</div>
+
+			<div className="card me-3">
+		  		<div className="card-body d-flex align-items-center justify-content-center p-0 bg-dark" style={{width: "7rem", fontSize:"5rem", color: "white"}}>{Math.floor(remainingSeconds / 10)}</div>
+			</div>
+
+			<div className="card me-3">
+		  		<div className="card-body d-flex align-items-center justify-content-center p-0 bg-dark" style={{width: "7rem", fontSize:"5rem", color: "white"}}>{remainingSeconds % 10}</div>
+			</div>
 		</div>
-	);
-};
+	  );
+	}
 
 export default Home;
